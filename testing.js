97e83ad7e436f10c024184fc8ca16e6cba5edb88
Henry Okoro <henry.okoro@iherb.com>
2021-09-07 11:52:32 -0700

    Changing Button handler

--- testing.js
+++ testing.js
@@ -1,6 +1,9 @@
 function myButtonHandler()
 {
     console.log("Bruh, button clicked")
+    myButton.classList.remove("is-danger")
+    myButton.classList.add("is-link")
+    myButton.textContent = 'My Test'
 }
 
 let myButton = document.getElementById("connectBtn")
