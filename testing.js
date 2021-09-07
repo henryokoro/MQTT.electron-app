7d28ef59d16e0e851091446d64f094b0fb3c60d4
Henry Okoro <henry.okoro@iherb.com>
2021-09-07 14:26:11 -0700

    display styles

--- testing.js
+++ testing.js
@@ -9,21 +9,27 @@ let content = document.getElementsByClassName("content")[0]
 //     myButton.classList.add("is-link")
 //     myButton.textContent = 'My Test'
 // }
+let counter = 0
 
 let myLambda = () => {
     console.log("Bruh, button clicked")
     myButton.classList.remove("is-danger")
     myButton.classList.add("is-link")
     myButton.textContent = 'My Test'
-    //Create new button
+    //Create new button (can only pass in tags that are html)
+    // let newDiv = document.createElement("div")
     let newButton = document.createElement("button")
-    newButton.textContent = "New Button"
+    newButton.textContent = "New Button " + counter
     content.appendChild(newButton)
     newButton.classList.add("is-primary")
     newButton.classList.add("button")
+    newButton.classList.add("new-line")
+
+    // newDiv.appendChild(newButton)
+    content.appendChild(newButton)
+    counter++
 
 }
-
 
 
 myButton.addEventListener("click", myLambda)
